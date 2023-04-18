e4d0802d297eef158570c80a24cbd6a25597b7e5
Олег Косенок <121554768+Yensygit@users.noreply.github.com>
2023-04-18 15:40:33 +0300

    Update weather_parser.py
    Удалена информация о влажности из за изменений на сайте

--- weather_parser.py
+++ weather_parser.py
@@ -12,8 +12,9 @@ def info_weather():
     :return: Словарь с данными о погоде если в БД имеются значения.
              None в случае если значений в БД нет
     """
-
-    current_time = f'{str(datetime.now().hour)}:{str(datetime.now().minute)}'
+    time = datetime.now()
+    # time = datetime(year=2023, month=2, day=17, hour=13, minute=2, second=12)
+    current_time = time.strftime('%H:%M')
     connection = create_connection("C:\Soft\Projects\PersonalWeatherBot\PWB_DB.sqlite")
     select_value_db = f"select * from subs_weather where time='{current_time}';"
     value_db = execute_read_query(connection, select_value_db)
@@ -42,17 +43,17 @@ def parser_today(values:tuple):
     info = BeautifulSoup(html_doc.text, 'html.parser')
     weather_dict = {'user_id': values[1], 'city': values[2]}
     value_today = info.find('a', class_="weathertab weathertab-block tooltip")
-    humidity = info.find_all('div', class_='widget-row widget-row-humidity')
-    humidity_list = [int(i.text) for i in humidity[0].contents]
-    humidity_average = sum(humidity_list) / len(humidity_list)
     wind = info.find_all('span', class_='wind-unit unit unit_wind_m_s')
-    wind_list = [int(i.text) for i in wind[16:]]
-    wind_max = max(wind_list)
+    wind_list = [int(i.text[0]) for i in wind]
+    if len(wind_list)>0:
+        wind_max = max(wind_list)
+    else:
+        wind_max = 'Безветренно'
     temp_min_max = value_today.find_all('span', class_='unit unit_temperature_c')
     day = value_today.find('div', class_='tab-content')
     day_text = day.contents[0].text
     condition = value_today.attrs['data-text']
-    weather_dict.update({'t_max': temp_min_max[1].text, 't_min': temp_min_max[0].text, 'day': day_text, 'format': 'Завтра','condition': condition, 'humidity_average': humidity_average, 'wind_max': wind_max})
+    weather_dict.update({'t_max': temp_min_max[1].text, 't_min': temp_min_max[0].text, 'day': day_text, 'format': 'Завтра','condition': condition, 'wind_max': wind_max})
     return weather_dict
 
 def parser_tomorrow(values:tuple):
@@ -68,15 +69,16 @@ def parser_tomorrow(values:tuple):
     info = BeautifulSoup(html_doc.text, 'html.parser')
     weather_dict = {'user_id':values[1], 'city':values[2]}
     value_tomorrow = info.find('a', class_="weathertab weathertab-block tooltip")
-    humidity = info.find_all('div', class_='widget-row widget-row-humidity')
-    humidity_list = [int(i.text) for i in humidity[0].contents]
-    humidity_average = sum(humidity_list)/len(humidity_list)
     wind = info.find_all('span', class_='wind-unit unit unit_wind_m_s')
-    wind_list = [int(i.text) for i in wind[16:]]
+    wind_list = [int(i.text) for i in wind]
+    if len(wind_list)>0:
+        wind_max = max(wind_list)
+    else:
+        wind_max = 'Безветренно'
     wind_max = max(wind_list)
     temp_min_max = value_tomorrow.find_all('span', class_='unit unit_temperature_c')
     day = value_tomorrow.find('div', class_='tab-content')
     day_text = day.contents[0].text
     condition = value_tomorrow.attrs['data-text']
-    weather_dict.update({'t_max': temp_min_max[1].text, 't_min':temp_min_max[0].text, 'day':day_text, 'format':'Завтра', 'condition':condition, 'humidity_average':humidity_average, 'wind_max':wind_max})
+    weather_dict.update({'t_max': temp_min_max[1].text, 't_min':temp_min_max[0].text, 'day':day_text, 'format':'Завтра', 'condition':condition, 'wind_max':wind_max})
     return weather_dict
